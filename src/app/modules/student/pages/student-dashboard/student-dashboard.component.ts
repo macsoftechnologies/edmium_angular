606b479b422e02ebb76ce0ce893b29adcc09f928
Ganesh Pathala <macsoftechnologies@gmail.com>
2020-08-05 19:12:16 +0530

    navigate student dashboard to universities list

--- src/app/modules/student/pages/student-dashboard/student-dashboard.component.ts
+++ src/app/modules/student/pages/student-dashboard/student-dashboard.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-student-dashboard',
-  templateUrl: './student-dashboard.component.html',
-  styleUrls: ['./student-dashboard.component.scss']
+  selector: "app-student-dashboard",
+  templateUrl: "./student-dashboard.component.html",
+  styleUrls: ["./student-dashboard.component.scss"],
 })
 export class StudentDashboardComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onNavigateToUniversityList() {
+    this.router.navigateByUrl("student/universities");
   }
-
 }
--- src/app/modules/student/student-routing.module.ts
+++ src/app/modules/student/student-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { StudentDashboardComponent } from "./pages/student-dashboard/student-dashboard.component";
+import { StudentUniversitiesListComponent } from "./pages/student-universities-list/student-universities-list.component";
 
 const routes: Routes = [
   { path: "dashboard", component: StudentDashboardComponent },
+  { path: "universities", component: StudentUniversitiesListComponent },
 ];
 
 @NgModule({
