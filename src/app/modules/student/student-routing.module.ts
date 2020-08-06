947347c50abc30d5fc6fa071fb2314e404c8bb22
Lohith .s <macsoftechnologies@gmail.com>
2020-08-06 11:39:59 +0530

    modified student dashboard

--- src/app/modules/student/student-routing.module.ts
+++ src/app/modules/student/student-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { StudentDashboardComponent } from "./pages/student-dashboard/student-dashboard.component";
 import { StudentUniversitiesListComponent } from "./pages/student-universities-list/student-universities-list.component";
+import { StudentProfileComponent } from './components/student-profile/student-profile.component';
 
 const routes: Routes = [
   { path: "dashboard", component: StudentDashboardComponent },
   { path: "universities", component: StudentUniversitiesListComponent },
+  { path: "studentprofile", component: StudentProfileComponent },
 ];
 
 @NgModule({
